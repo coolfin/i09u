c9e18e91d2069c2cd3208f98bd22bee53f0f0fae
8ark <sorlti6952@gmail.com>
2023-03-19 12:33:28 +0900

    FECH people data
    add People Data
    add Corp company data
    
    hv to add more Corp data

--- data.ts
+++ data.ts
@@ -4,6 +4,33 @@ import { HistoryType } from './@types/entity.d';
 
 export const factories: FactoryType[] = [
   {
+    name: '야마와(Yamawa)',
+    eng_name: 'Yamawa(야마와)',
+    path: '/images/company_logo/bosch.png',
+    url: 'http://www.yamawa.co.kr/',
+  },
+
+  {
+    name: '한국야금(Korloy)',
+    eng_name: 'Korloy(한국야금)',
+    path: '/images/company_logo/yg.png',
+    url: 'http://www.korloy.com/ko/main/main.do',
+  },
+
+  {
+    name: '대구텍(Taegutec)',
+    eng_name: 'Taegutec(대구텍)',
+    path: '/images/company_logo/makita.png',
+    url: 'https://www.taegutec.com/pages/',
+  },
+
+  {
+    name: '미쓰도요(Mitutoyo)',
+    eng_name: 'Mitutoyo(미쓰도요)',
+    path: '/images/company_logo/stanley.png',
+    url: 'http://www.mitutoyokorea.com/',
+  },
+  {
     name: '보쉬(BOSCH)',
     eng_name: 'BOSCH(보쉬)',
     path: '/images/company_logo/bosch.png',
@@ -31,31 +58,28 @@ export const factories: FactoryType[] = [
     url: 'https://kr.stanleytools.global/ko-KR',
   },
   {
-    name: '보쉬(BOSCH)',
-    eng_name: 'BOSCH(보쉬)',
-    path: '/images/company_logo/bosch.png',
-    url: 'https://www.bosch.co.kr/',
-  },
-
-  {
-    name: '와이지(YG)',
-    eng_name: 'YG(와이지)',
-    path: '/images/company_logo/yg.png',
-    url: 'https://www.yg1.kr/kor/main.asp',
-  },
-
-  {
-    name: '마키타(Makita)',
-    eng_name: 'Makita(마키타)',
-    path: '/images/company_logo/makita.png',
-    url: 'https://www.lkmakita.co.kr/main.html',
-  },
-
-  {
-    name: '스탠리(Stanley)',
-    eng_name: 'Stanley(스탠리)',
+    name: '타지마(Tajima)',
+    eng_name: 'Tajima(타지마)',
     path: '/images/company_logo/stanley.png',
-    url: 'https://kr.stanleytools.global/ko-KR',
+    url: 'http://www.koreatajima.com/',
+  },
+  {
+    name: '스마토(Smato)',
+    eng_name: 'Smato(스마토)',
+    path: '/images/company_logo/stanley.png',
+    url: 'https://www.smatotool.com/product/main.do',
+  },
+  {
+    name: '킹토니(Kingtony)',
+    eng_name: 'Kingtony(킹토니)',
+    path: '/images/company_logo/stanley.png',
+    url: 'https://www.kingtony.com/',
+  },
+  {
+    name: '오토스(Otos)',
+    eng_name: 'Otos(오토스)',
+    path: '/images/company_logo/stanley.png',
+    url: 'https://www.otos.co.kr/',
   },
 ];
 
@@ -69,19 +93,43 @@ export const people: PeopleType[] = [
   {
     name: '김미란',
     eng_name: 'Miran Kim',
-    job: 'Director',
+    job: 'CEO(CO)',
     url: '/images/icon/people2.png',
   },
   {
-    name: '박상우',
-    eng_name: 'Sangwoo Park',
-    job: 'CTO',
+    name: '박현희',
+    eng_name: 'Hyunhee Park',
+    job: 'Manager',
     url: '/images/icon/people1.png',
   },
   {
+    name: '강나정',
+    eng_name: 'Najung Kang',
+    job: 'Assistant Manager',
+    url: '/images/icon/people3.png',
+  },
+  {
+    name: '김하린',
+    eng_name: 'Harin Kim',
+    job: 'Assistant Manager',
+    url: '/images/icon/people2.png',
+  },
+  {
+    name: '김지철',
+    eng_name: 'Jichul Kim',
+    job: 'Assistant Manager',
+    url: '/images/icon/people3.png',
+  },
+  {
     name: '박아연',
-    eng_name: 'Ayeon Park',
-    job: 'Intern',
+    eng_name: 'Ahyeon Park',
+    job: 'Assistant Manager',
+    url: '/images/icon/people2.png',
+  },
+  {
+    name: '김민아',
+    eng_name: 'Minah Kim',
+    job: 'Assistant Manager',
     url: '/images/icon/people3.png',
   },
 ];
